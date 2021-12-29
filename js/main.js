35a0efb00a3e894c2e0c3b73942b2aed4e1130c9
Luis Tapia <devlou@protonmail.com>
2021-12-28 23:04:08 -0500

    Set up a hover effect so that the grid divs change color when the mouse passes over them

--- js/main.js
+++ js/main.js
@@ -11,8 +11,16 @@ let totalSquares = getTotalSquares(rows, columns);
 let sketchpadWidth = sketchpadStyles.width;
 let sketchpadHeight = sketchpadStyles.height;
 
-let squareDivWidth = getPx(sketchpadWidth) / columns;
-let squareDivHeight = getPx(sketchpadHeight) / rows;
+const sketchpadBorder = sketchpadStyles.border;
+const sketchpadBorderPxStr = findPxValue(sketchpadBorder);
+const sketchpadBorderPxNum = getPxNum(sketchpadBorderPxStr);
+
+let lineColor = getRGB(0, 0, 0);
+
+let squareDivWidth =
+  (getPxNum(sketchpadWidth) - sketchpadBorderPxNum * 2) / columns;
+let squareDivHeight =
+  (getPxNum(sketchpadHeight) - sketchpadBorderPxNum * 2) / rows;
 
 for (let i = 0; i < totalSquares; i++) {
   createSquareDiv(
@@ -22,10 +30,18 @@ for (let i = 0; i < totalSquares; i++) {
   );
 }
 
+const squareDivs = document.querySelectorAll(".square-div");
+
+squareDivs.forEach((square) => {
+  square.addEventListener("mouseover", () => {
+    square.style.background = lineColor;
+  });
+});
+
 function createSquareDiv(node, height, width) {
   const squareDiv = document.createElement("div");
   squareDiv.classList.add("square-div");
-  squareDiv.setAttribute("style", `height: ${height}; width: ${width}`);
+  squareDiv.style.cssText = `height: ${height}; width: ${width}`;
   node.appendChild(squareDiv);
 }
 
@@ -33,10 +49,25 @@ function getTotalSquares(rows, columns) {
   return rows * columns;
 }
 
-function getPx(str) {
+function getPxNum(str) {
   return +str.slice(0, -2);
 }
 
 function getUnitStr(num, unit) {
   return `${num}${unit}`;
+}
+
+function getRGB(num1, num2, num3) {
+  return `rgb(${num1}, ${num2}, ${num3})`;
+}
+
+function findPxValue(cssRule) {
+  const items = cssRule.split(" ");
+  for (let i = 0; i < items.length; i++) {
+    const lastTwoChars = items[i].slice(-2);
+    const firstChars = items[i].slice(0, -2);
+    if (lastTwoChars === "px" && items[i].length > 2 && +firstChars !== NaN) {
+      return items[i];
+    }
+  }
 }
