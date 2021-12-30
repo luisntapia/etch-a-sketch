4bdd121f3e715531be44c09cb97396cb2b0c5e9e
Luis Tapia <devlou@protonmail.com>
2021-12-30 00:57:56 -0500

    Add button to clear the sketchpad and form to resize pixels

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@
 const sketchpad = document.querySelector("#sketchpad");
 const sketchpadStyles = getComputedStyle(sketchpad);
 
-let rows = 16;
-let columns = 16;
+const btnClear = document.querySelector("#clear-btn");
+const btnResizePixels = document.querySelector("#pixels-btn");
 
-let totalSquares = getTotalSquares(rows, columns);
+const inputRows = document.querySelector("#rows");
+const inputColumns = document.querySelector("#columns");
+
+const gridInputs = document.querySelectorAll(".grid");
+
+let sketchpadRows = 16;
+let sketchpadColumns = 16;
+
+let totalSquares = getTotalSquares(sketchpadRows, sketchpadColumns);
 
 let sketchpadWidth = sketchpadStyles.width;
 let sketchpadHeight = sketchpadStyles.height;
@@ -18,17 +26,29 @@ const sketchpadBorderPxNum = getPxNum(sketchpadBorderPxStr);
 let lineColor = getRGB(0, 0, 0);
 
 let squareDivWidth =
-  (getPxNum(sketchpadWidth) - sketchpadBorderPxNum * 2) / columns;
+  (getPxNum(sketchpadWidth) - sketchpadBorderPxNum * 2) / sketchpadColumns;
 let squareDivHeight =
-  (getPxNum(sketchpadHeight) - sketchpadBorderPxNum * 2) / rows;
+  (getPxNum(sketchpadWidth) - sketchpadBorderPxNum * 2) / sketchpadRows;
 
-for (let i = 0; i < totalSquares; i++) {
-  createSquareDiv(
-    sketchpad,
-    getUnitStr(squareDivHeight, "px"),
-    getUnitStr(squareDivWidth, "px")
-  );
-}
+gridInputs.forEach((input) => {
+  input.addEventListener("input", () => {
+    const maxValue = +input.max;
+    const minValue = +input.min;
+    if (+input.value > maxValue) {
+      input.value = maxValue;
+    }
+    if (+input.value < minValue) {
+      input.value = minValue;
+    }
+  });
+});
+
+fillWithSquares(
+  sketchpad,
+  totalSquares,
+  getUnitStr(squareDivWidth, "px"),
+  getUnitStr(squareDivHeight, "px")
+);
 
 const squareDivs = document.querySelectorAll(".square-div");
 
@@ -36,6 +56,14 @@ squareDivs.forEach((square) => {
   square.addEventListener("mouseover", () => {
     square.style.background = lineColor;
   });
+});
+
+btnClear.addEventListener("click", clearAllSquareDivs);
+
+btnResizePixels.addEventListener("click", () => {
+  const rows = +inputRows.value;
+  const columns = +inputColumns.value;
+  updateRowsAndColumns(rows, columns);
 });
 
 function createSquareDiv(node, height, width) {
@@ -70,4 +98,66 @@ function findPxValue(cssRule) {
       return items[i];
     }
   }
+}
+
+function clearAllSquareDivs() {
+  changeBackground(".square-div", "none");
+}
+
+function changeBackground(cls, bgColor) {
+  const items = document.querySelectorAll(cls);
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.background = bgColor;
+  }
+}
+
+function updateRowsAndColumns(rows, columns) {
+  sketchpadRows = rows;
+  sketchpadColumns = columns;
+  const squareQuantity = rows * columns;
+  const width = getSquareDivsLength(rows);
+  const height = getSquareDivsLength(columns);
+
+  removeAllFromParent(sketchpad, ".square-div");
+  fillWithSquares(sketchpad, squareQuantity, width, height);
+  const squareDivs = document.querySelectorAll(".square-div");
+  squareDivs.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.background = lineColor;
+    });
+  });
+}
+
+function updateWidth(cls, width) {
+  const items = document.querySelectorAll(cls);
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.width = width;
+  }
+}
+
+function updateHeight(cls, height) {
+  const items = document.querySelectorAll(cls);
+  for (let i = 0; i < items.length; i++) {
+    items[i].style.height = height;
+  }
+}
+
+function removeAllFromParent(parentNode, cls) {
+  const items = parentNode.querySelectorAll(cls);
+  for (let i = 0; i < items.length; i++) {
+    parentNode.removeChild(items[i]);
+  }
+}
+
+function fillWithSquares(node, squares, height, width) {
+  for (let i = 0; i < squares; i++) {
+    createSquareDiv(node, height, width);
+  }
+}
+
+function getSquareDivsLength(rowsOrColumns) {
+  return getUnitStr(
+    (getPxNum(sketchpadWidth) - sketchpadBorderPxNum * 2) / rowsOrColumns,
+    "px"
+  );
 }
